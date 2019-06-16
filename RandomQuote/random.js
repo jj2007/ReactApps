fff0aad0fb3fb23391f81c76be39103b29601e46
jj2007 <jjayeshjadhav@yahoo.com>
2019-06-16 10:30:55 +0530

    Author added as well

--- RandomQuote/random.js
+++ RandomQuote/random.js
@@ -2,11 +2,12 @@ class RandomQuote extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      myquotes: [],
-      selectedQuote: null
+      myquotes: ["Winning isn’t everything, but wanting to win is."],
+      selectedQuote: "",
+      author: "",
+      clicked: false
     };
   }
-
   componentDidMount() {
     fetch(
       "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
@@ -14,25 +15,25 @@ class RandomQuote extends React.Component {
       .then(response => response.json())
       .then(data => this.setState({ myquotes: data.quotes }));
   }
-  handleSubmit() {
+
+  handleClick = () => {
+    const rand = Math.floor(Math.random() * this.state.myquotes.length);
     this.setState({
-      clicked: true
+      clicked: true,
+      selectedQuote: this.state.myquotes[rand].quote,
+      author: this.state.myquotes[rand].author
     });
-  }
+  };
   render() {
     const { myquotes } = this.state;
     return (
-      <React.Fragment>
-        <ul>
-          {myquotes.map(quote => (
-            <li key={quote.author}>
-              <p>
-                {quote.quote} BY {quote.author}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <div id="quote-box">
+        <div>
+          <h1 id="text">{this.state.selectedQuote}</h1>
+          <h1 id="author">{this.state.author}</h1>
+        </div>
+        <button onClick={this.handleClick}>NewQuote</button>
+      </div>
     );
   }
 }
