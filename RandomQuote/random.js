392eeea9dc3bf524dda918a605e25d0c34788236
jj2007 <jjayeshjadhav@yahoo.com>
2019-06-16 07:10:26 +0530

    Fetch API promise working

--- RandomQuote/random.js
+++ RandomQuote/random.js
@@ -1,28 +1,38 @@
-class Quote extends React.Component {
+class RandomQuote extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: ""
+      myquotes: [],
+      selectedQuote: null
     };
   }
 
-  render() {
-    return (
-      <div id="text">
-        <p>{this.state.quote}</p>
-      </div>
-    );
+  componentDidMount() {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then(response => response.json())
+      .then(data => this.setState({ myquotes: data.quotes }));
   }
-}
-
-class RandomQuote extends React.Component {
+  handleSubmit() {
+    this.setState({
+      clicked: true
+    });
+  }
   render() {
+    const { myquotes } = this.state;
     return (
-      <div id="quote-box">
-        <Quote showNextQuote={this.props} />
-        <Qtweet />
-        <NewQuote />
-      </div>
+      <React.Fragment>
+        <ul>
+          {myquotes.map(quote => (
+            <li key={quote.author}>
+              <p>
+                {quote.quote} BY {quote.author}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
     );
   }
 }
