e61accd03ebb9ea2b353242df2b143a6504f0ea8
jj2007 <jjayeshjadhav@yahoo.com>
2019-06-23 14:35:15 +0530

    Pomodoro clock modified :tada:

--- pomodoroClock/pomo.js
+++ pomodoroClock/pomo.js
@@ -7,49 +7,78 @@ class Pomodoro extends React.Component {
       session: 25
     };
   }
+  handleBreakUp = () => {
+    if (this.state.breaks !== 60) {
+      this.setState({
+        breaks: parseInt(this.state.breaks, 10) + 1
+      });
+    }
+  };
+  handleBreakDown = () => {
+    if (this.state.breaks !== 5) {
+      this.setState({
+        breaks: parseInt(this.state.breaks, 10) - 1
+      });
+    }
+  };
+  handleSessionUp = () => {
+    if (this.state.session !== 60) {
+      this.setState({
+        session: parseInt(this.state.session, 10) + 1
+      });
+    }
+  };
+  handleSessionDown = () => {
+    if (this.state.session !== 25) {
+      this.setState({
+        session: parseInt(this.state.session, 10) - 1
+      });
+    }
+  };
   render() {
     return (
       <div id="clock">
-        <header>Pomodoro Clock</header>
+        <header>
+          <h1>Pomodoro Clock</h1>
+        </header>
         <div id="break-label">
           <h2>Break Length</h2>
-          <Display>{this.state.breaks}</Display>
-          <Button>
-            <i class="fas fa-arrow-circle-up" />
+          <button className="btn btn-danger" onClick={this.handleBreakUp}>
+            <i className="fas fa-arrow-circle-up" />
             Up
-          </Button>
-          <Button>
-            <i class="fas fa-arrow-circle-down" />
+          </button>
+          <Display>{this.state.breaks}</Display>
+          <button className="btn btn-danger" onClick={this.handleBreakDown}>
+            <i className="fas fa-arrow-circle-down" />
             Down
-          </Button>
+          </button>
         </div>
         <div id="session-label">
           <h2>Session Length</h2>
-          <Display>{this.state.session}</Display>
-          <Button>
-            <i class="fas fa-arrow-circle-up" />
+          <button className="btn btn-danger" onClick={this.handleSessionUp}>
+            <i className="fas fa-arrow-circle-up" />
             Up
-          </Button>
-          <Button>
-            <i class="fas fa-arrow-circle-down" />
+          </button>
+          <Display>{this.state.session}</Display>
+          <button className="btn btn-danger" onClick={this.handleSessionDown}>
+            <i className="fas fa-arrow-circle-down" />
             Down
-          </Button>
+          </button>
         </div>
         <Timer
           id="timer-label"
           breaks={this.state.breaks}
           session={this.state.session}
         />
-        <PlayPause id="controls" />
+        <PlayPause
+          id="controls"
+          breaks={this.state.breaks}
+          session={this.state.session}
+        />
       </div>
     );
   }
 }
-const Button = ({ id, className, children }) => (
-  <button id={id} className={className}>
-    {children}
-  </button>
-);
 
 class Timer extends React.Component {
   constructor(props) {
@@ -71,12 +100,25 @@ class PlayPause extends React.Component {
   constructor(props) {
     super(props);
   }
+  startTimer = () => {};
+  stopTimer = () => {};
+  resetTimer = () => {};
   render() {
+    const { id, breaks, sessionsl } = this.props;
     return (
-      <div>
-        <button id="start_stop">play</button>
-        <button id="start_stop">pause</button>
-        <button id="reset">reset</button>
+      <div id={id}>
+        <button id="start_stop" class="btn" onClick={this.startTimer}>
+          <i class="fas fa-play-circle" />
+          play
+        </button>
+        <button id="start_stop" class="btn" onClick={this.stopTimer}>
+          <i class="fas fa-pause-circle" />
+          pause
+        </button>
+        <button id="reset" class="btn" onClick={this.resetTimer}>
+          <i class="fas fa-sync-alt" />
+          reset
+        </button>
       </div>
     );
   }
